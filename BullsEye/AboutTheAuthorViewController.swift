a3dcd3b75d543eb2c8201afbd941f1c6c7770a87
John Page <jdehpage@gmail.com>
2019-06-15 18:33:21 +0100

    About game and about author views added

---
+++ BullsEye/AboutTheAuthorViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AboutTheAuthorViewController.swift
+//  BullsEye
+//
+//  Created by John Daniel Page on 14/06/2019.
+//  Copyright © 2019 JohnPage. All rights reserved.
+//
+
+import UIKit
+
+class AboutTheAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+}
