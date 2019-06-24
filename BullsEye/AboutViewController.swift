7b92f1ea87f9c7a93d440a1021e93658cac249e6
John Page <jdehpage@gmail.com>
2019-06-24 18:14:21 +0100

    About View Fixed

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by John Daniel Page on 24/06/2019.
+//  Copyright © 2019 JohnPage. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
